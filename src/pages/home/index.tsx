a95d61eb61536b9d5c3df244e38d63c0db20624a
Fernando de Row <ferdr89dev@gmail.com>
2025-01-09 12:17:26 -0300

    feat: add CharacterCard and Pagination in Home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,28 +1,43 @@
-import { useNavigate } from "react-router";
+// import { useNavigate } from "react-router";
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getCharactersBy } from "@/services";
+import CharacterCard from "@/components/character-card";
+import Pagination from "@/components/pagination";
+import { Character } from "@/types";
+
+const param = "";
 
 const Home = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
+  const [page, setPage] = useState<number>(1);
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["characters", page, param],
+    queryFn: () => getCharactersBy(page, param),
+    retry: 1,
+  });
+
+  //TODO: Loader (?)
+  if (isLoading) return <h2>Loading...</h2>;
+
+  //TODO: Error component (?)
+  if (error) return <h2>Oops, hubo un error</h2>;
+
   return (
     <div>
-      Home
-      <button
-        style={{ width: "200px", height: "50px" }}
-        onClick={() => navigate("/character/1")}
-      >
-        Character
-      </button>
-      <button
-        style={{ width: "200px", height: "50px" }}
-        onClick={() => navigate("/favorites")}
-      >
-        Favorites
-      </button>
-      <button
-        style={{ width: "200px", height: "50px" }}
-        onClick={() => navigate("/NOEXISTE")}
-      >
-        Not-Found
-      </button>
+      {data &&
+        data?.results.map((character: Character) => (
+          <CharacterCard key={character.id} {...character} />
+        ))}
+
+      {data?.info && (
+        <Pagination
+          totalPages={data?.info.pages}
+          currentPage={page}
+          setPage={setPage}
+        />
+      )}
     </div>
   );
 };
