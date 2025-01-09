4cb550691e48fc34c2e657e31c331215a3fc84c2
Fernando de Row <ferdr89dev@gmail.com>
2025-01-09 15:59:31 -0300

    feat: add CharacterDetail component

---
+++ src/components/character-detail/index.tsx
@@ -0,0 +1,75 @@
+import { Character, Episode } from "@/types";
+
+type CharacterDetailProps = Omit<
+  Character,
+  "id" | "episode" | "url" | "created"
+> & { episodes: Episode[] };
+
+const CharacterDetail = ({
+  name,
+  image,
+  status,
+  species,
+  type,
+  gender,
+  origin,
+  location,
+  episodes,
+}: CharacterDetailProps) => {
+  return (
+    <article>
+      <h2>{name}</h2>
+      <img
+        src={image}
+        alt={"Image of: " + name}
+        loading="lazy"
+        width="300px"
+        height="300px"
+      />
+      <p>
+        <span>Status: </span>
+        {status}
+      </p>
+      <p>
+        <span>Specie: </span>
+        {species}
+      </p>
+
+      {type && (
+        <p>
+          <span>Type: </span>
+          {type}
+        </p>
+      )}
+      <p>
+        <span>Gender: </span>
+        {gender}
+      </p>
+      <p>
+        <span>Origin: </span>
+        {origin?.name}
+      </p>
+      <p>
+        <span>Location: </span>
+        {location?.name}
+      </p>
+
+      <h4>Episodes</h4>
+      <ul>
+        {episodes.map(({ id, name, episode }) => (
+          <li key={id} style={{ display: "flex" }}>
+            <p>
+              <span>name: </span>
+              {name}
+            </p>
+            <p>
+              <span>number: </span>
+              {episode}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </article>
+  );
+};
+export default CharacterDetail;
