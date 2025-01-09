ce4a51213453809aeac29a2972838484db7efdda
Fernando de Row <ferdr89dev@gmail.com>
2025-01-09 11:45:25 -0300

    refactor: add Episode interface and remove export in Location interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -13,7 +13,7 @@ export interface Character {
   created: Date;
 }
 
-export interface Location {
+interface Location {
   name: string;
   url: string;
 }
@@ -32,4 +32,14 @@ interface InfoClass {
   pages: number;
   next: string;
   prev: null;
+}
+
+export interface Episode {
+  id: number;
+  name: string;
+  air_date: string;
+  episode: string;
+  characters: string[];
+  url: string;
+  created: Date;
 }
