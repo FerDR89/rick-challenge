22c9bd1358674cd8cd0b0fff129da3b4e5903b80
Fernando de Row <ferdr89dev@gmail.com>
2025-01-11 12:34:20 -0300

    feat: add lazy loading, Spinner as fallback and Layout in router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,17 +1,25 @@
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router";
-import Character from "@/pages/character";
-import Favorites from "@/pages/favorites";
 import Home from "@/pages/home";
-import NotFound from "@/pages/notFound";
+import Layout from "@/components/layout/index.";
+import Spinner from "@/components/spinner";
+
+const Character = lazy(() => import("@/pages/character"));
+const Favorites = lazy(() => import("@/pages/favorites"));
+const NotFound = lazy(() => import("@/pages/notFound"));
 
 const MyRouter = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/character/:id" element={<Character />} />
-      <Route path="/favorites" element={<Favorites />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <Suspense fallback={<Spinner size="250" />}>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/character/:id" element={<Character />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
