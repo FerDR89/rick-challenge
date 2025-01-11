3d3253091c7108b424a2f1003e0e8b4089f3045a
Fernando de Row <ferdr89dev@gmail.com>
2025-01-11 00:25:12 -0300

    feat: add useCharactersQueryBiId, CharacterdCard and reset favorite characters button in Favorites page

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -1,29 +1,45 @@
+import Button from "@/components/button";
+import CharacterCard from "@/components/characterCard";
+import { useCharactersQueryById } from "@/hooks";
+import useFavoriteCharacters from "@/store/favoriteCharacters.store";
 import { useNavigate } from "react-router";
 
 const Favorites = () => {
   const navigate = useNavigate();
 
+  const removetAllCharacters = useFavoriteCharacters(
+    (state) => state.removetAllCharactersId
+  );
+
+  const charactersIds = useFavoriteCharacters((state) => state.charactersIds);
+
+  const {
+    data: characters,
+    isLoading,
+    error,
+  } = useCharactersQueryById(charactersIds);
+
   return (
     <div>
-      Favorites
-      <button
-        style={{ width: "200px", height: "50px" }}
-        onClick={() => navigate("/character/1")}
-      >
-        Character
-      </button>
-      <button
-        style={{ width: "200px", height: "50px" }}
-        onClick={() => navigate("/")}
-      >
+      <Button style={{ width: "150px" }} onClick={() => navigate("/")}>
         Home
-      </button>
-      <button
-        style={{ width: "200px", height: "50px" }}
-        onClick={() => navigate("/NOEXISTE")}
-      >
-        Not-Found
-      </button>
+      </Button>
+
+      {isLoading && <h2>Loading...</h2>}
+      {error && <h2>Oops, hubo un error</h2>}
+
+      {charactersIds.length === 0 && (
+        <h2>There are no favorite characters yet</h2>
+      )}
+
+      {characters.length > 0 &&
+        characters.map((character) => (
+          <CharacterCard key={character.id} {...character} />
+        ))}
+
+      <Button style={{ width: "150px" }} onClick={() => removetAllCharacters()}>
+        Reset Favorites
+      </Button>
     </div>
   );
 };
