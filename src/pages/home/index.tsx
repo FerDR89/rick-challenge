09a14830f18b5b33ae8e566e15ff0636bd76ae70
Fernando de Row <ferdr89dev@gmail.com>
2025-01-10 22:23:23 -0300

    feat: add useFilterParam hook, handles filters and search, add navigation to favorites and reset filters button in Home Page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,31 +1,76 @@
-// import { useNavigate } from "react-router";
 import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
-import { getCharactersBy } from "@/services";
-import CharacterCard from "@/components/character-card";
+import { Link } from "react-router";
+import CharacterCard from "@/components/characterCard";
 import Pagination from "@/components/pagination";
-import { Character } from "@/types";
-
-const param = "";
+import SearchBar from "@/components/searchBar";
+import SelectFilter from "@/components/selectFilter";
+import Button from "@/components/button";
+import useFilterParam from "@/store/filterParams.store.";
+import { useCharactersQuery } from "@/hooks";
+import { Character, Species, Status } from "@/types";
 
 const Home = () => {
-  // const navigate = useNavigate();
   const [page, setPage] = useState<number>(1);
+  const query = useFilterParam((state) => state.query);
+  const setQuery = useFilterParam((state) => state.setQuery);
+  const resetQueries = useFilterParam((state) => state.resetQueries);
+  const { data, isLoading, error } = useCharactersQuery(page, query);
 
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["characters", page, param],
-    queryFn: () => getCharactersBy(page, param),
-    retry: 1,
-  });
+  const onStatusFilter = (value: Status) => {
+    setQuery({ status: value });
+    setPage(1);
+  };
 
-  //TODO: Loader (?)
-  if (isLoading) return <h2>Loading...</h2>;
+  const onSpeciesFilter = (value: Species) => {
+    setQuery({ species: value });
+    setPage(1);
+  };
 
-  //TODO: Error component (?)
-  if (error) return <h2>Oops, hubo un error</h2>;
+  const onSearch = (value: string) => {
+    setQuery({ name: value });
+    setPage(1);
+  };
 
   return (
     <div>
+      <p>{JSON.stringify(query)}</p>
+
+      <Link to="/favorites">
+        <Button style={{ width: "150px" }}>FAVORITES</Button>
+      </Link>
+
+      <SearchBar onSearch={onSearch} />
+
+      <SelectFilter
+        onSelectChange={onSpeciesFilter}
+        options={[
+          "Human",
+          "Alien",
+          "Humanoid",
+          "unknown",
+          "Poopybutthole",
+          "Mythological Creature",
+          "Animal",
+          "Robot",
+          "Cronenberg",
+          "Disease",
+        ]}
+        value={query.species}
+      />
+
+      <SelectFilter
+        onSelectChange={onStatusFilter}
+        options={["Alive", "Dead", "Unknown"]}
+        value={query.status}
+      />
+
+      <Button style={{ width: "150px" }} onClick={() => resetQueries()}>
+        Reset filters
+      </Button>
+
+      {isLoading && <h2>Loading...</h2>}
+      {error && <h2>Oops, hubo un error</h2>}
+
       {data &&
         data?.results.map((character: Character) => (
           <CharacterCard key={character.id} {...character} />
