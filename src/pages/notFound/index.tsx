7007530b32c053a373266ba53ef56babc130b5fc
Fernando de Row <ferdr89dev@gmail.com>
2025-01-12 16:19:56 -0300

    feat: add 404 image, add Text and styles in NotFound page

--- src/pages/notFound/index.tsx
+++ src/pages/notFound/index.tsx
@@ -1,18 +1,37 @@
-import { useNavigate } from "react-router";
+import Text from "@/components/text";
+import styles from "./notFound.module.css";
 
 const NotFound = () => {
-  const navigate = useNavigate();
-
   return (
-    <div>
-      NOT FOUND
-      <button
-        style={{ width: "200px", height: "50px" }}
-        onClick={() => navigate("/")}
-      >
-        HOME
-      </button>
-    </div>
+    <article className={styles.notFound__container}>
+      <Text
+        tag="subtitle"
+        text="I don't know Rick"
+        style={{
+          color: "#60a85f",
+          textShadow: "1px 1px 3px #88e23b, 5px 5px 5px #000",
+          fontSize: "clamp(32px, 5vw, 48px)",
+        }}
+      />
+      <Text
+        tag="subtitle"
+        text="This seems like a"
+        style={{
+          color: "#60a85f",
+          textShadow: "1px 1px 3px #88e23b, 5px 5px 5px #000",
+          fontSize: "clamp(32px, 5vw, 48px)",
+        }}
+      />
+      <Text
+        tag="subtitle"
+        text="Falsy value."
+        style={{
+          color: "#60a85f",
+          textShadow: "1px 1px 3px #88e23b, 5px 5px 5px #000",
+          fontSize: "clamp(32px, 5vw, 48px)",
+        }}
+      />
+    </article>
   );
 };
 export default NotFound;
