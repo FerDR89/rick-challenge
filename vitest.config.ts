456d6bd93593fa84e1fbca38f413b3488f22f29f
Fernando de Row <ferdr89dev@gmail.com>
2025-01-13 12:13:15 -0300

    chore: add classNameStrategy in vitest.config

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,11 @@ export default defineConfig({
     environment: "jsdom",
     globals: true,
     setupFiles: "./src/test/setup.ts",
+    css: {
+      modules: {
+        classNameStrategy: "non-scoped",
+      },
+    },
   },
   resolve: {
     alias: {
