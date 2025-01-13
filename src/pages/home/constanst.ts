05922da628c673e5869484712e7c131cf2ffb2ca
Fernando de Row <ferdr89dev@gmail.com>
2025-01-12 21:18:44 -0300

    chore: extract select options in a constanst file in Home page

---
+++ src/pages/home/constanst.ts
@@ -0,0 +1,17 @@
+import { Species, Status } from "@/types";
+
+const filterStatusOptions: Status[] = ["Alive", "Dead", "Unknown"];
+const filterSpeciesOptions: Species[] = [
+  "Human",
+  "Alien",
+  "Humanoid",
+  "unknown",
+  "Poopybutthole",
+  "Mythological Creature",
+  "Animal",
+  "Robot",
+  "Cronenberg",
+  "Disease",
+];
+
+export { filterStatusOptions, filterSpeciesOptions };
